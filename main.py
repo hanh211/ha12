38fd728b0d8cea2b4a82cdd2ce4b5facc03dcabd
hanh <hanh@gmail.com>
2023-10-05 21:37:46 +0700

    a

--- main.py
+++ main.py
@@ -1,84 +1,43 @@
-#!/usr/bin/env python
-
-import kivy
-import lovecal
-
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.properties import StringProperty
-from kivy.graphics import Color, Rectangle
-from kivy.core.audio import SoundLoader
-from kivy.uix.textinput import TextInput
-from kivy.core.window import Window
-from kivy.uix.bubble import Bubble
+import time
+Builder.load_string('''
+<CameraClick>:
+    orientation: 'vertical'
+    Camera:
+        id: camera
+        resolution: (640, 480)
+        play: False
+    ToggleButton:
+        text: 'Play'
+        on_press: camera.play = not camera.play
+        size_hint_y: None
+        height: '48dp'
+    Button:
+        text: 'Capture'
+        size_hint_y: None
+        height: '48dp'
+        on_press: root.capture()
+''')
 
 
-class ValidateInput(TextInput):
-    #invalid_set = r"/\*:|'.?" + '"<>'
-
-    invalid_set = r"[-.\'@_!#$%^&*()<>?/\|}{~:0123456789 ]"
-    count = 0
-
-    def insert_text(self, substring, from_undo=False):
-        firstname = [c for c in substring if c not in self.invalid_set]
-        s = ''.join(firstname)
-        if not self.filled:
-            return super().insert_text(s, from_undo=from_undo)
-
-
-#    def keyboard_on_key_up(self, keycode, text):
-#        if self.readonly and text[1] == "backspace":
-#            self.readonly = False
-#            self.do_backspace()
-
-class Manager(ScreenManager):
-
-    result = StringProperty(None)
-    percentage = StringProperty(None)
-
-    def __init__(self, **kwargs):
-        super(Manager, self).__init__(**kwargs)
-
-    def playstart(self):
-        sound = SoundLoader.load('sound/click.wav')
-        sound.play()
-
-    def playreturn(self):
-        sound = SoundLoader.load('sound/click.wav')
-        sound.play()
+class CameraClick(BoxLayout):
+    def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        camera.export_to_png("IMG_{}.png".format(timestr))
+        print("Captured")
 
 
-    def results(self):
-        sound = SoundLoader.load('sound/click.wav')
-        print(self.ids.firstname.text)
-        print(self.ids.secondname.text)
-        first = self.ids.firstname.text
-        second = self.ids.secondname.text
-        results = lovecal.calculator(first, second)
-        #print(results)
-        if results == "Failed":
-            self.result = "Failed"
-            self.percentage = "Failed"
-        else:
-            self.result = results['result']
-            self.percentage = results['percentage']
-        #self.result = results[1]
-        #self.percentage = results[0]
-        print(self.result)
-        print(self.percentage)
-        #print(str(results['percentage']))
-        #print(str(results['result']))
-        sound.play()
+class TestCamera(App):
 
-class ScreenApp(App):
     def build(self):
-        self.icon = 'images/appicon.png'
-        self.sm = Manager()
-        return self.sm
+        return CameraClick()
 
-Window.softinput_mode = "below_target"
 
-if __name__ == '__main__':
-    ScreenApp().run()
+TestCamera().run()
